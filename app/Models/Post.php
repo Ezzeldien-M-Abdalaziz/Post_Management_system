b6299b38d7bf5c7abaddafb91e2630d31c1e4ba2
Ezzeldien-M-Abdalaziz <ezzeldien.m.abdalziz@gmail.com>
2025-05-30 16:00:06 +0300

    eloquent

---
+++ app/Models/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'content',
+        'visibility',
+        'user_id',
+    ];
+
+    /**
+     * Get the user that owns the post.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Scope a query to only include visible posts.
+     */
+    public function scopeVisible($query)
+    {
+        return $query->where('visibility', true);
+    }
+
+}
